3ef3e76727a9019acf8ca2bc48ab39ed19c5add0
Richard Hofmänner <hello@terillos.dev>
2021-01-29 17:49:25 +0100

    Sync

--- routes/v1/index.ts
+++ routes/v1/index.ts
@@ -1,6 +1,8 @@
 import {OakRouter} from "../../deps.ts";
 
 const v1Router = new OakRouter({prefix: '/v1'})
+const gameId: number = 509670
+let appAccessToken: undefined|string = undefined
 
 v1Router.get('/', (ctx) => {
   ctx.response.body = {
@@ -8,6 +10,16 @@ v1Router.get('/', (ctx) => {
   }
 })
 
-v1Router.get('')
+v1Router.get('/streams', async (ctx) => {
+  if (appAccessToken === undefined) {
+    const result = await fetch('https://id.twitch.tv/oauth2/token' +
+      `?client_id=${Deno.env.get('TWITCH_CLIENT_ID')}` +
+      `&client_secret=${Deno.env.get('TWITCH_CLIENT_SECRET')}` +
+      '&grant_type=client_credentials', {
+      method: 'POST'
+    });
+    
+  }
+})
 
 export default v1Router
