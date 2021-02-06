459c04342ef23ce9739cc352f6d1310c09f07bd0
Richard Hofmänner <hello@terillos.dev>
2021-02-06 17:50:14 +0100

    Fixed Error after rewrite

--- helpers/TwitchAuth.ts
+++ helpers/TwitchAuth.ts
@@ -1,20 +1,24 @@
 export default class TwitchAuth {
   static getAccessToken(): Promise<string> {
     return new Promise((resolve, reject) => {
-      fetch('https://id.twitch.tv/oauth2/token' +
-        `?client_id=${Deno.env.get('TWITCH_CLIENT_ID')}` +
-        `&client_secret=${Deno.env.get('TWITCH_CLIENT_SECRET')}` +
-        '&grant_type=client_credentials', {
-        method: 'POST'
-      }).then((result: Response) => {
-        if (result.status === 200 && result.body !== null) {
-          result.json().then((response) => {
-            resolve(<string>response.access_token)
-          })
-        } else {
-          reject('Error while fetching Access Token at Twitch.')
-        }
-      });
+      try {
+        fetch('https://id.twitch.tv/oauth2/token' +
+            `?client_id=${Deno.env.get('TWITCH_CLIENT_ID')}` +
+            `&client_secret=${Deno.env.get('TWITCH_CLIENT_SECRET')}` +
+            '&grant_type=client_credentials', {
+          method: 'POST'
+        }).then((result: Response) => {
+          if (result.status === 200 && result.body !== null) {
+            result.json().then((response) => {
+              resolve(<string>response.access_token)
+            })
+          } else {
+            reject('Error while fetching Access Token at Twitch.')
+          }
+        });
+      } catch (e) {
+        reject(e)
+      }
     })
   }
 }
--- helpers/twitchApi/Streams.ts
+++ helpers/twitchApi/Streams.ts
@@ -1,30 +1,46 @@
 import {Stream} from "../../typings/Stream.ts";
 
 export class Streams {
-  static getStreamsByGameId(gameId: string, appAccessToken: string, limit: number = 20, offset: number = 0): Promise<any> {
-    let streams: Array<Stream> = []
-    let cursor: string|null = null
-    return new Promise( async (resolve, reject) => {
-      do {
-        let requestUrl = `https://api.twitch.tv/helix/streams?game_id=${gameId}&first=100`
-        if (cursor !== null) {
-          requestUrl = `${requestUrl}&after=${cursor}`
+  private readonly clientId: string
+  private readonly appAccessToken: string
+
+  constructor(clientId: string, appAccessToken: string) {
+    this.clientId = clientId
+    this.appAccessToken = appAccessToken
+  }
+
+  getStreamsByGameId(gameId: string, limit = 20, cursor = ""): Promise<any> {
+    return new Promise((resolve, reject) => {
+      try {
+        if (limit > 100) {
+          limit = 20
         }
-        const result = await fetch(requestUrl, {
+
+        let requestUrl = `https://api.twitch.tv/helix/streams?first=${limit}`
+        if (cursor !== "") {
+          requestUrl += `&after=${cursor}`
+        }
+        fetch(requestUrl, {
           headers: {
-            'Client-Id': `${Deno.env.get('TWITCH_CLIENT_ID')}`,
-            'Authorization': `Bearer ${appAccessToken}`
+            'Client-Id': this.clientId,
+            'Authorization': `Bearer ${this.appAccessToken}`
           }
         })
-        if (result.status === 200 && result.body !== null) {
-          const response = await result.json()
-          streams.push.apply(response.data)
-          cursor = response.pagination.cursor
-        } else {
-          reject('Something went wrong while fetching the streams.')
-          break;
-        }
-      } while (streams.length % 100 === 0)
+            .then((result) => {
+              if (result.status === 200 && result.body !== null) {
+                result.json().then((response) => {
+                  resolve(response)
+                })
+              } else {
+                result.json().then((response) => {
+                  console.log(response)
+                })
+                reject('Something went wrong while fetching the streams')
+              }
+            })
+      } catch (e) {
+        console.log(e)
+      }
     })
   }
 }
