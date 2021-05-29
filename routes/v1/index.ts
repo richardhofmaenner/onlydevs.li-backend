ae99fc99e8c50af1b70eba8bd99a23d26cf902d5
Richard Hofmänner <hello@terillos.dev>
2021-05-29 20:39:59 +0200

    Splitted Routes into seprate files

--- routes/v1/index.ts
+++ routes/v1/index.ts
@@ -1,48 +1,11 @@
 import {OakRouter, OakContext} from "../../deps.ts";
-import TwitchAuth from '../../helpers/TwitchAuth.ts'
-import {StreamSearchParams} from "../../typings/StreamSearchParams.ts";
-import {Streams as StreamApi} from "../../helpers/twitchApi/Streams.ts";
 
 const v1Router = new OakRouter({prefix: '/v1'})
-const gameId = "509670"
-let appAccessToken: undefined | string = undefined
 
 v1Router.get('/', (ctx: OakContext) => {
   ctx.response.body = {
     'version': 1.0
   }
-})
-
-v1Router.get('/streams', async (ctx: OakContext) => {
-
-  const queryParams: StreamSearchParams = {
-    limit: <number|null>ctx.request.url.searchParams.get('limit'),
-    cursor: <string|null>ctx.request.url.searchParams.get('cursor')
-  }
-
-  const limit = (queryParams.limit === null)? 20 : queryParams.limit
-  const cursor = (queryParams.cursor === null)? "": queryParams.cursor
-
-  if (appAccessToken === undefined) {
-    await TwitchAuth.getAccessToken()
-      .then((access_token: string) => {
-        appAccessToken = access_token
-      })
-      .catch((err: string) => {
-        ctx.response.status = 500
-        ctx.response.body = {"error": err}
-      })
-  }
-
-  const streamApi = new StreamApi(<string>Deno.env.get('TWITCH_CLIENT_ID'), <string>appAccessToken)
-  await streamApi.getStreamsByGameId(gameId, limit, cursor)
-    .then((res) => {
-      ctx.response.body = res
-    })
-    .catch((error) => {
-      ctx.response.status = 500
-      ctx.response.body = {"error": error}
-    })
 })
 
 export default v1Router
