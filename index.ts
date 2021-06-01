d649991d333af9d5342afc2d07988cf768613187
Richard Hofmänner <hello@terillos.dev>
2021-06-01 17:42:09 +0200

    Added Tags Route

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { OakApp } from "./deps.ts"
 import v1Router from "./routes/v1/index.ts"
 import streamsRouter from "./routes/v1/streams.ts";
+import tagsRouter from "./routes/v1/tags.ts";
 
 const app = new OakApp()
 
@@ -8,6 +9,8 @@ app.use(v1Router.routes())
 app.use(v1Router.allowedMethods())
 app.use(streamsRouter.routes())
 app.use(streamsRouter.allowedMethods())
+app.use(tagsRouter.routes())
+app.use(tagsRouter.allowedMethods())
 
 app.addEventListener('listen', () => {
   console.log(`App has started and is listening!`)
